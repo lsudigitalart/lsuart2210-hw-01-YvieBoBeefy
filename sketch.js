d27a43d459ac543df2658f20020aead1cdfd0608
Katie Wilson <Katie Wilson>
2016-08-25 11:31:39 -0500

    done

---
+++ sketch.js
@@ -0,0 +1,43 @@
+function setup() {
+  createCanvas(1200, 600);
+   background(0)
+
+  ellipseMode(CENTER);
+   fill(255);
+    ellipse(100, 115, 80);
+    ellipse(1100, 450, 450);
+
+  ellipseMode(CENTER);
+   fill(0);
+    ellipse(710, 500, 700);
+
+  ellipseMode(CENTER);
+   fill(255);
+    ellipse(700, 500, 700);
+
+  ellipseMode(CENTER);
+   fill(0);
+    ellipse(980, 600, 500);
+
+  ellipseMode(CENTER);
+   fill(255);
+    ellipse(1000, 600, 500, 500);
+
+  ellipseMode(CENTER);
+   fill(0);
+    ellipse(310, 650, 300);
+
+  ellipseMode(CENTER);
+   fill(255);
+    ellipse(150, 210, 60);
+    ellipse(300, 650, 300);
+    ellipse(225, 285, 50);
+    ellipse(325, 325, 45);
+    ellipse(75, 0, 100);
+
+
+
+}
+function draw() {
+
+}
